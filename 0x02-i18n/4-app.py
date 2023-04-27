a11c4a1c00a903a4f02bcc81d3547e976cec3c6a
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2023-04-28 00:25:50 +0300

    additional lines

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
-""" 0. Basic Flask app """
-from flask import Flask, render_template, request
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
-def get_locale():
-    """ get_locale """
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        print(locale)
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-class Config():
-    """ Config class for Babel"""
+class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    # BABEL_LOCALE_SELECTOR = get_locale
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
-# Either define it this like  or with it defined in the
-# config object
-# babel.init_app(app, locale_selector=get_locale)
-babel.init_app(app, locale_selector=get_locale)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def index():
-    """ index """
-    return render_template("4-index.html", get_locale=get_locale)
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
-
+    app.run(host='0.0.0.0', port=5000)
