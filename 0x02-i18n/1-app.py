992dc59ced4fcae579bcf0741814ecaf886078c3
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2023-04-28 00:23:02 +0300

    changes

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
-""" 0. Basic Flask app """
-from flask import Flask, render_template
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
-class Config():
-    """ Config class for Babel"""
-    LANGUAGE = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = LANGUAGE[0]
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-babel = Babel(app)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @app.route('/')
-def index():
-    """ index """
-    return render_template("1-index.html")
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-
+    app.run(host='0.0.0.0', port=5000)
